6714d099c96dc6df00fd79ae11bb99dba70d4cdd
moulina pradhan <pmoulina76@gmail.com>
2020-10-27 21:02:16 +0530

    added ratings of the productS

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+const App=()=> {
+  return (
+    <div className="App">
+      <Header/>
+      <main className="py-3">
+      <Container>
+     <HomeScreen/>
+      </Container>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
