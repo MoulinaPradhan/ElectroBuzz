7f1b4a7cf75ab3d48cbf2bce508c3357d2035f7c
moulina pradhan <pmoulina76@gmail.com>
2020-10-30 12:19:20 +0530

    add package colors to color the consoler

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
+import connectDB from "./config/db.js"
+import colors from 'colors';
 
 dotenv.config()
 const app = express();
+
+connectDB()
 
 app.get('/',(req,res)=>{
     res.send("API is running...")
@@ -21,4 +25,4 @@ app.get('/api/products/:id',(req,res)=>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
