27b82fd2b8d476c24b225e0602821c7de967dfb1
moulina pradhan <pmoulina76@gmail.com>
2020-10-31 22:06:25 +0530

    add bcrypt to encode password

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,20 +1,25 @@
+import bcrypt from 'bcryptjs'
+
 const users=[
     {
         name:'Admin User',
         email:'admin@example.com',
-        password:xxxx,
+        password:bcrypt.hashSync('123456',10),
         isAdmin:true
     },
     {
         name:'John Doe',
         email:'john@example.com',
-        password:xxxx,
+        password:bcrypt.hashSync('123456',10),
         
     },
     {
         name:'Jane Doe',
         email:'jane@example.com',
-        password:xxxx,
+        password:bcrypt.hashSync('123456',10),
         
     },
 ]
+
+
+export default users
