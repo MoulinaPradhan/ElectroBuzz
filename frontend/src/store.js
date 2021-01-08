8ef3801a0b59a1c99c3c5e6e786994cc3873064e
moulina pradhan <pmoulina76@gmail.com>
2021-01-08 23:34:58 +0530

    shipping screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -26,8 +26,13 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse
 (localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse
+(localStorage.getItem('shippingAddress')) : {}
+
 const initialState={
-    cart: { cartItems:cartItemsFromStorage},
+    cart: { cartItems:cartItemsFromStorage ,
+        shippingAddress: shippingAddressFromStorage
+    },
     userLogin :{ userInfo : userInfoFromStorage}
 }
 
