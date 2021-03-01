dfe01164d4174607adc6904d547be53dfa1b5e89
moulina pradhan <pmoulina76@gmail.com>
2021-03-01 23:23:22 +0530

    changed to production mode

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send("API is running...")
-})
+// app.get('/',(req,res)=>{
+//     res.send("API is running...")
+// })
 
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
@@ -33,8 +33,22 @@ app.use('/api/upload',uploadRoutes)
 app.get('/api/config/paypal', (req , res) => 
 res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+    app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html')));
+
+}else{
+    app.get('/',(req,res)=>{
+    res.send("API is running...")
+})
+}
+
 
 app.use(notFound)
 
---
+++ frontend/build/precache-manifest.d924b8c887f37637f7ab49450b391c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de988aa2b64db6dce1479049ada2e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b618472e18db6da6913",
+    "url": "/static/css/main.16ac91ce.chunk.css"
+  },
+  {
+    "revision": "848d326ee388fdd9bb26",
+    "url": "/static/js/2.9fdfd4ff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9fdfd4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b618472e18db6da6913",
+    "url": "/static/js/main.b226e6db.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
