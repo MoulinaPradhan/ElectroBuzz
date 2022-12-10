d9f25455ee13febf92ebfcb0afd0d401453d51cf
MoulinaPradhan <pmoulina76@gmail.com>
2022-12-10 16:56:54 +0530

    Port changed to 5000

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ app.use(notFound)
 app.use(errorHandler)
 
 //need to add later
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 connectDB().then(()=>{
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
