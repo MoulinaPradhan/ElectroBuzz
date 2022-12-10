edda1c15d5bc62270c1bcfbebb6e46ae1e7116d3
Moulina Pradhan <pmoulina76@gmail.com>
2022-12-10 15:34:30 +0530

    Added cyclic.sh

--- backend/server.js
+++ backend/server.js
@@ -56,4 +56,6 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
+connectDB().then(()=>{
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+})
