6a10ba88af847bfa145e3a62d9a13ce0a83699ab
MoulinaPradhan <pmoulina76@gmail.com>
2023-04-09 19:42:42 +0530

    add build folder

---
+++ frontend/build/precache-manifest.52732b2dc838f07a69af1f34e7679aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0547584a243760b8019db23d0c4ad334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eb4d230739648ee193b",
+    "url": "/static/css/main.38e189ff.chunk.css"
+  },
+  {
+    "revision": "64d59a0307a2d3f95523",
+    "url": "/static/js/2.b0bc5fbd.chunk.js"
+  },
+  {
+    "revision": "c4381524cae000488e2fb889012466d0",
+    "url": "/static/js/2.b0bc5fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb4d230739648ee193b",
+    "url": "/static/js/main.bb7e10a1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
